9e95c2da5e6dbc76e657398a6e57865818d77b7b
Eric Chen <Eric@Erics-MacBook-Pro.local>
2017-05-24 10:59:37 -0400

    Created directory for all pokemon classes, created starter pokemon classes.

---
+++ Pokemon/Squirtle.java
@@ -0,0 +1,11 @@
+public class Squirtle extends Pokemon {
+
+    public Squirtle() {
+	
+	// Pokemon( int index, String name, int lvl, Move move1, Move move2, Move move3, Move move4 )
+	// Move( String name, int pp, int dmg, int accuracy, int stat, int boost )
+	super( 7, "Squirtle", 5, new Move( "Tackle", 35, 50, 100, 0, 0 ), new Move( "Tail Whip", 30, 0, 100, 1, 5 ), null, null );
+	
+    }
+
+}
