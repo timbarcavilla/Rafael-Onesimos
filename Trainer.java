9ae84f58bfbdf8a8b83b0558a292808109130993
Tim Wang <zwang6@stuy.edu>
2017-05-24 10:48:33 -0400

    meghan trainer

--- Trainer.java
+++ Trainer.java
@@ -1,4 +1,19 @@
 public abstract class Trainer {
-
     
+    protected ArrayList<Pokemon> _party;
+    protected String _name;
+
+    public boolean hasPokemon(){
+	return _party.size() != 0;
+    }
+
+    public String getName(){
+	return _name;
+    }
+
+    public String setName(String newName){
+	String retstr = _name;
+	_name = newName;
+	return retstr;
+    }
 }
