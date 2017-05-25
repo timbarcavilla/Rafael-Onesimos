83776279c27e7190b57e4677a515891e41bdf077
awang18 <awang14@stuy.edu>
2017-05-25 10:42:24 -0400

    modified bulbasaur and added geodude

--- Pokemon/Bulbasaur.java
+++ Pokemon/Bulbasaur.java
@@ -4,7 +4,9 @@ public class Bulbasaur extends Pokemon {
 	
 	// Pokemon( int index, String name, int lvl, Move move1, Move move2, Move move3, Move move4 )
 	//  Move( String name, int pp, int dmg, int accuracy, int category, int stat, int boost )
-	super( 4, "Bulbasaur", 5, new Move( "Tackle", 35, 50, 100, 0, 0, 0 ), new Move( "Growl", 40, 0, 100, 0, 2, 5 ), null, null );
+	super( 1, "Bulbasaur", 5, new Move( "Tackle", 35, 50, 100, 0, 0, 0 ), new Move( "Growl", 40, 0, 100, 0, 2, 5 ), new Move("Vine Whip", 25, 45, 100, 0, 0, 0), null );
+	hasEvo = true;
+	evoLvl = 16;
 	
     }
 
---
+++ Pokemon/Geodude.java
@@ -0,0 +1,7 @@
+public class Geodude extends Pokemon {
+
+    public Geodude() {
+	
+        // Pokemon( int index, String name, int lvl, Move move1, Move move2, Move move3, Move move4 )
+	//  Move( String name, int pp, int dmg, int accuracy, int category, int stat, int boost )
+	super(
