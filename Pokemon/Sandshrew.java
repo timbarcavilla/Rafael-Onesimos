2a6ad04a236ae043e0cfbf9bcb6f6afca6e73746
Eric <echen8@stuy.edu>
2017-05-25 11:00:08 -0400

    Replaced Pokemon folder with poke

--- Pokemon/Sandshrew.java
+++
@@ -1,9 +0,0 @@
-public Sandshrew extends Pokemon {
-
-    public Sandshrew() {
-	
-	// Pokemon( int index, String name, int lvl, Move move1, Move move2, Move move3, Move move4 )
-	//  Move( String name, int pp, int dmg, int accuracy, int category, int stat, int boost )
-	super( 27, "Sandshrew", 5, new Move( "Scratch", 35, 40, 100, 0, 0, 0), null, null, null);
-    }
-}
