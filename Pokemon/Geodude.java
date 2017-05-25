5d97364a03b2bb4d361d77fc74b693da1f684c06
awang18 <awang14@stuy.edu>
2017-05-25 10:50:00 -0400

    added sandshrew

--- Pokemon/Geodude.java
+++ Pokemon/Geodude.java
@@ -4,4 +4,7 @@ public class Geodude extends Pokemon {
 	
         // Pokemon( int index, String name, int lvl, Move move1, Move move2, Move move3, Move move4 )
 	//  Move( String name, int pp, int dmg, int accuracy, int category, int stat, int boost )
-	super(
+	super(74, "Geodude", 5, new Move("Tackle", 35, 50, 100, 0, 0, 0 ), null, null, null );
+
+    }
+}
