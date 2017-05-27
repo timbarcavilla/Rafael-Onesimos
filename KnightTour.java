60be61787b17942bdd17cd318aa632ba9e6d5a2c
timbarcavilla <zwang6@stuy.edu>
2017-05-27 14:45:29 -0400

    some stuff

---
+++ KnightTour.java
@@ -0,0 +1,19 @@
+public class KnightTour extends Puzzle{
+
+    public KnightTour(int newSize){
+	_size = newSize;
+	status = false;
+    }
+
+    public boolean checkSolution(int _solution){
+
+	//implementation
+
+    }
+
+    public void draw(){
+
+	//implementation
+
+    }
+}
---
+++ QueenBoard.java
@@ -0,0 +1,19 @@
+public class QueenBoard extends Puzzle{
+
+    public QueenBoard(int newSize){
+	_size = newSize;
+	status = false;
+    }
+
+    public boolean checkSolution(int _solution){
+
+	//implementation
+
+    }
+
+    public void draw(){
+
+	//implementation
+
+    }
+}
